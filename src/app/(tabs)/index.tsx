66e06ab70cd53e3f06c22c7195acc21b543a5548
Antony Jaison <antonyjaison639@gmail.com>
2024-02-19 21:42:02 +0530

    2. First component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,40 +1,16 @@
-import { StyleSheet, Image } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
-import Colors from '@/src/constants/Colors';
 import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
 
-export default function TabOneScreen() {
+
+
+export default function MenuScreen() {
   const product = products[0];
   return (
-    <View style={styles.container}>
-      <Image source={{ uri : product.image}} style={styles.image}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-      
+    <View>
+      <ProductListItem product={product}/>
+      <ProductListItem product={product}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor:"#fff",
-    padding:10,
-    borderRadius:20
-  },
-  title: {
-    fontSize:20,
-    fontWeight:'bold',
-  },
-  price:{
-    fontSize:18,
-    fontWeight:'600',
-    color:Colors.light.tint
-  },
-  image:{
-    width:'100%',
-    aspectRatio:1,
-    resizeMode:'contain'
-  }
-});
